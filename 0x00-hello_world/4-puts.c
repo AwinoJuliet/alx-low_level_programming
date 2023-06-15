c34db19b4b7a477a0bbc774965cdc4cffd85b8b9
AwinoJuliet <emmajulietawino14.com>
2023-06-15 08:51:59 -0700

    printing c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main-print puts func string
+ *
+ *Description:use func main
+ *this program prints "programming is like building a multilingual puzzle
+ *Return:0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
