c0c4389f873bb5d819fad4057a877d2148612694
AwinoJuliet <emmajulietawino14.com>
2023-06-16 01:12:03 -0700

    computer size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-
 int main(void)
 {
-    char D;
-    int A;
-    long int B;
-    float F;
-
+char D;
+int A;
+long int B;
+float F;
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(D));
 printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(A));
 printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(B));
 printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(F));
 return (0);
 }
-
