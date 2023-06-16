c2679520cbb0a53c8d6afec50895615cec5c0378
AwinoJuliet <emmajulietawino14.com>
2023-06-16 01:20:51 -0700

    computers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(A));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(B));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(C));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(F));
-return 0;
+return (0);
 }
