b21a858e58509841c7630224897b59629f0f00f2
AwinoJuliet <emmajulietawino14.com>
2023-06-16 00:29:35 -0700

    size6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int main(void){
+int main(void)
+{
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long));
