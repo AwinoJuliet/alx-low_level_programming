77d6796ea9186798aa38337ded49db53577c0f2c
AwinoJuliet <emmajulietawino14.com>
2023-06-16 01:18:02 -0700

    computers sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,10 +4,12 @@ int main(void)
 char D;
 int A;
 long int B;
+long long int C;
 float F;
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(D));
-printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(A));
-printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(B));
-printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(F));
-return (0);
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(D));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(A));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(B));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(C));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(F));
+return 0;
 }
