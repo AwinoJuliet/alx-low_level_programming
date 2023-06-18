e12c7b9b1895eb030dead14ff3e60059a3abb937
AwinoJuliet <emmajulietawino14.com>
2023-06-18 03:48:35 -0700

    print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+		putchar(number + '0');
+
+	putchar('\n');
+
+	return (0);
+}
