029640226dd11ffd3a007caa422f162ce2ff785a
AwinoJuliet <emmajulietawino14.com>
2023-06-18 04:32:29 -0700

    fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,100 +7,20 @@
  */
 int main(void)
 {
-	putchar('0');
-	putchar(',');
-	putchar(' ');
+	int i;
 
-	putchar('0' + 1);
-	putchar(',');
-	putchar(' ');
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
 
-	putchar('0' + 2);
-	putchar(',');
-	putchar(' ');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
-	putchar('0' + 3);
-	putchar(',');
-	putchar(' ');
-
-	putchar('0' + 4);
-	putchar(',');
-	putchar(' ');
-
-	putchar('0' + 5);
-	putchar(',');
-	putchar(' ');
-
-	putchar('0' + 6);
-	putchar(',');
-	putchar(' ');
-
-	putchar('0' + 7);
-	putchar(',');
-	putchar(' ');
-
-	putchar('0' + 8);
-	putchar(',');
-	putchar(' ');
-
-	putchar('0' + 9);
-	putchar(',');
-	putchar(' ');
-
-	putchar('1');
-	putchar('0');
-	putchar(',');
-	putchar(' ');
-
-	putchar('1');
-	putchar('1');
-	putchar(',');
-	putchar(' ');
-
-	putchar('1');
-	putchar('2');
-	putchar(',');
-	putchar(' ');
-
-	putchar('1');
-	putchar('3');
-	putchar(',');
-	putchar(' ');
-
-	putchar('1');
-	putchar('4');
-	putchar(',');
-	putchar(' ');
-
-	putchar('1');
-	putchar('5');
-	putchar(',');
-	putchar(' ');
-
-	putchar('1');
-	putchar('6');
-	putchar(',');
-	putchar(' ');
-
-	putchar('1');
-	putchar('7');
-	putchar(',');
-	putchar(' ');
-
-	putchar('1');
-	putchar('8');
-	putchar(',');
-	putchar(' ');
-
-	putchar('1');
-	putchar('9');
-	putchar(',');
-	putchar(' ');
-
-	putchar('2');
-	putchar('0');
 	putchar('\n');
 
 	return (0);
 }
-
