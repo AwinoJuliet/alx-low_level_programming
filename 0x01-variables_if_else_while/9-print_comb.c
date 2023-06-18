5aef8e22fbd54c6fc0408b51c57457d52a3f7063
AwinoJuliet <emmajulietawino14.com>
2023-06-18 04:16:55 -0700

    priny combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('0');
+	putchar(',');
+	putchar(' ');
+
+	putchar('0' + 1);
+	putchar(',');
+	putchar(' ');
+
+	putchar('0' + 2);
+	putchar(',');
+	putchar(' ');
+
+	putchar('0' + 3);
+	putchar(',');
+	putchar(' ');
+
+	putchar('0' + 4);
+	putchar(',');
+	putchar(' ');
+
+	putchar('0' + 5);
+	putchar(',');
+	putchar(' ');
+
+	putchar('0' + 6);
+	putchar(',');
+	putchar(' ');
+
+	putchar('0' + 7);
+	putchar(',');
+	putchar(' ');
+
+	putchar('0' + 8);
+	putchar(',');
+	putchar(' ');
+
+	putchar('0' + 9);
+	putchar(',');
+	putchar(' ');
+
+	putchar('1');
+	putchar('0');
+	putchar(',');
+	putchar(' ');
+
+	putchar('1');
+	putchar('1');
+	putchar(',');
+	putchar(' ');
+
+	putchar('1');
+	putchar('2');
+	putchar(',');
+	putchar(' ');
+
+	putchar('1');
+	putchar('3');
+	putchar(',');
+	putchar(' ');
+
+	putchar('1');
+	putchar('4');
+	putchar(',');
+	putchar(' ');
+
+	putchar('1');
+	putchar('5');
+	putchar(',');
+	putchar(' ');
+
+	putchar('1');
+	putchar('6');
+	putchar(',');
+	putchar(' ');
+
+	putchar('1');
+	putchar('7');
+	putchar(',');
+	putchar(' ');
+
+	putchar('1');
+	putchar('8');
+	putchar(',');
+	putchar(' ');
+
+	putchar('1');
+	putchar('9');
+	putchar(',');
+	putchar(' ');
+
+	putchar('2');
+	putchar('0');
+	putchar('\n');
+
+	return (0);
+}
+
