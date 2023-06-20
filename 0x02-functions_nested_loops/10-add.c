bdcbe05a600db7a2151230c109a1b317edaa6602
AwinoJuliet <emmajulietawino14.com>
2023-06-20 01:33:30 -0700

    adding intergers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+
+#include "main.h"
+/**
+ *add-description add two nums
+ *@num1:parameters to be added
+ *@num2:param1
+ *Return:0 (success)
+ */
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
