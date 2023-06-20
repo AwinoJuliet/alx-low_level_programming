871ae518e49e1c62053f6287d09c43b9c04e6fdb
AwinoJuliet <emmajulietawino14.com>
2023-06-20 01:22:48 -0700

    checking every minute

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer.
+ */
+void jack_bauer(void)
+{
+int hour, minute;
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+printf("%02d:%02d\n", hour, minute);
+}
+}
+}
+int main(void)
+{
+jack_bauer();
+return 0;
+}
