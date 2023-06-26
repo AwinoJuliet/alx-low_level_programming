8fa11215443c80eee7c787455c76a05b00d6fc29
AwinoJuliet <emmajulietawino14.com>
2023-06-26 02:21:52 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+/**
+ * main - Generates a random valid password for the 101-crackme program.
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+    char password[PASSWORD_LENGTH + 1];
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    int i;
+
+    srand(time(NULL)); // Initialize the random number generator
+
+    // Generate random characters for the password
+    for (i = 0; i < PASSWORD_LENGTH; i++)
+    {
+        int index = rand() % (sizeof(charset) - 1);
+        password[i] = charset[index];
+    }
+
+    password[PASSWORD_LENGTH] = '\0'; // Add null terminator
+
+    printf("%s\n", password);
+
+    return 0;
+}
