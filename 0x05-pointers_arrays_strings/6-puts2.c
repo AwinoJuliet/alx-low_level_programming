c3604ab5e40d393bd6e2a5f8af35942c89ad66d2
AwinoJuliet <emmajulietawino14.com>
2023-06-26 01:43:31 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "unistd.h"
+/**
+ *puts2 - reverse q string
+ *@str: The string to be reversed
+ */
+void puts2(char *str)
+{
+int len = 0, index = 0;
+while (str[index++])
+{
+len++;
+}
+for (index = 0 ; index < len ; index += 2)
+{
+_putchar(str[index]);
+}
+_putchar('\n');
+}
