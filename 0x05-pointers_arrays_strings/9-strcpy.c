662a34dfb928eaae8cadfdd67081d00c71f5d224
AwinoJuliet <emmajulietawino14.com>
2023-06-26 02:08:04 -0700

    9strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,27 +1,19 @@
 #include "main.h"
-#include <unistd.h>
-
+#include "unistd.h"
 /**
- * _strcpy - Reverse a string and copy it to another buffer.
- * @dest: The buffer to copy the reversed string to.
- * @src: The source string to be reversed.
+ *_strcpy - reverse q string
+ *@dest: buffer tocopy string to
+ *@src: the source string tocopy
  *
- * Return: A pointer to the destination buffer.
+ *Return: a pointer to destination
  */
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
-    int src_len = 0;
-    int i;
-
-    // Calculate the length of the source string
-    while (src[src_len] != '\0')
-        src_len++;
-
-    // Copy characters from source string to destination in reverse order
-    for (i = 0; i < src_len; i++)
-        dest[i] = src[src_len - i - 1];
-
-    dest[i] = '\0'; // Add null terminator to the destination string
-
-    return dest;
+int index = 0;
+while (src[index])
+{
+dest[index] = src[index];
+index++;
+}
+return (dest);
 }
