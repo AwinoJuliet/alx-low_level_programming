e47c2ee35d2354cff13487e8efaa1a643b2f7826
AwinoJuliet <emmajulietawino14.com>
2023-06-26 03:26:55 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,31 +8,27 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int i = 0;
+    int sign = 1;
+    int result = 0;
+    int i = 0;
 
-	if (s[0] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (s[0] == '+')
-	{
-		i++;
-	}
+    if (s[0] == '-')
+    {
+        sign = -1;
+        i++;
+    }
 
-	for (; s[i] != '\0'; i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result = result * 10 + (s[i] - '0');
-		}
-		else if (result > 0)
-		{
-			break;
-		}
-	}
+    for (; s[i] != '\0'; i++)
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            result = result * 10 + (s[i] - '0');
+        }
+        else if (result > 0)
+        {
+            break;
+        }
+    }
 
-	return result * sign;
+    return result * sign;
 }
