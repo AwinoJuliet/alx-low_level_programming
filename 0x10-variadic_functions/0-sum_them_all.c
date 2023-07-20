e05897f1fa718a9577385afaace4ed4a538a077b
AwinoJuliet <emmajulietawino14.com>
2023-07-20 13:58:29 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,18 +9,21 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
-    int sum = 0;
+	va_list args;
+	unsigned int i;
+	int sum = 0;
 
-    va_start(args, n);
+	/* Initialize the argument list */
+	va_start(args, n);
 
-    for (i = 0; i < n; i++)
-    {
-        sum += va_arg(args, int);
-    }
+	/* Sum all the arguments */
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
 
-    va_end(args);
+	/* Clean up the argument list */
+	va_end(args);
 
-    return sum;
+	return sum;
 }
